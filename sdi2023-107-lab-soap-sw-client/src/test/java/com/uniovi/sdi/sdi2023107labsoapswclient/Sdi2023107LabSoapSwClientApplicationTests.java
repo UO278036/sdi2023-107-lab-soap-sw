882665ae3e428f6360a962c88f7a921a8672bd9e
Raúl España <uo278036@uniovi.es>
2023-04-24 00:14:52 +0200

    SDI-2223-107-SW-11.1- Servicio SOAP Cliente.

---
+++ sdi2023-107-lab-soap-sw-client/src/test/java/com/uniovi/sdi/sdi2023107labsoapswclient/Sdi2023107LabSoapSwClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.sdi2023107labsoapswclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2023107LabSoapSwClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
