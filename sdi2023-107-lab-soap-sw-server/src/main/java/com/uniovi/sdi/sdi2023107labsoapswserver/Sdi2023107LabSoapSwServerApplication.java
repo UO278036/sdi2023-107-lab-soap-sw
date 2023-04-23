5a6e13e46ecab7b6d38fb80b51456b028c97e1a9
Raúl España <uo278036@uniovi.es>
2023-04-23 22:19:07 +0200

    SDI-2223-107-SW-11.1-Servicio-SOAP-Server.

---
+++ sdi2023-107-lab-soap-sw-server/src/main/java/com/uniovi/sdi/sdi2023107labsoapswserver/Sdi2023107LabSoapSwServerApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.sdi2023107labsoapswserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2023107LabSoapSwServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2023107LabSoapSwServerApplication.class, args);
+	}
+
+}
